c94361714e5d70ac395cef8dd6c0dd8f962f0c43
ugliestcerberus <league@WTS-iMac-11.attlocal.net>
2018-09-22 14:29:57 -0700

    matthew has worked hard today.

--- src/_02_Firework_Display/Firework.java
+++ src/_02_Firework_Display/Firework.java
@@ -1,5 +1,6 @@
 package _02_Firework_Display;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
 
@@ -36,48 +37,65 @@ public class Firework {
 		// 4. Iterate through the sparks and reset their x and y location
 		// to their original starting point.
 		for (int i = 0; i < 100; i++) {
-			sparks[i].x;
-			sparks[i].y;
+			sparks[i].x =100;
+			//sparks[i].x;
+			//sparks[i].y;
+			//sparks[i]=x;
+			//sparks[i]=y;
+			sparks[i].y =100;
 		}
 	}
 
 	public void updateSparks() {
 		// 8. Uncomment this code. See if you can understand what it is doing.
 
-		// for(int i = 0; i < sparks.length; i++) {
+		for(int i = 0; i < sparks.length; i++) {
+		Spark s = sparks[i];
+		s.x += s.xVelocity;
+		s.y += s.yVelocity;
+		s.yVelocity += GRAVITY;
+		
+		//This code is telling us that for every spark, it increases the x Velocity when x increases
+		//The y velocity would increase when the y value increases
+		
+		if(s.yVelocity >= 0 && !s.fire) {
+		Random r = new Random();
+		s.xVelocity = r.nextInt(40) - 20;
+		s.yVelocity += -r.nextInt(10) - 5;
+		s.size = r.nextInt(7) + 2;
+		s.fire = true;
+		}
+		
+		//This code tells me that if the y Velocity is greater than or equal to o, then the spark will not fire. 
+		
+		if(s.y >= FireworkDisplay.HEIGHT) {
+		s.dead = true;
+		}
+		}
+		
+		//This code shows that if the sparks reach beyond the minimum or maximum height, then it would kill the spark
 		//
-		// Spark s = sparks[i];
-		// s.x += s.xVelocity;
-		// s.y += s.yVelocity;
-		// s.yVelocity += GRAVITY;
-		//
-		// if(s.yVelocity >= 0 && !s.fire) {
-		// Random r = new Random();
-		// s.xVelocity = r.nextInt(40) - 20;
-		// s.yVelocity += -r.nextInt(10) - 5;
-		// s.size = r.nextInt(7) + 2;
-		// s.fire = true;
-		// }
-		//
-		// if(s.y >= FireworkDisplay.HEIGHT) {
-		// s.dead = true;
-		// }
-		// }
-		//
-		// for(int i = 0; i < sparks.length; i++) {
-		// if(!sparks[i].dead) {
-		// dead = false;
-		// break;
-		// }
-		// dead = true;
-		// }
+		for(int i = 0; i < sparks.length; i++) {
+		if(!sparks[i].dead) {
+		dead = false;
+		break;
+		}
+		dead = true;
+		// This code shows that if the spark is not dead, then it would break apart so the spark can die. 
+		}
 	}
 
 	public void drawSparks(Graphics g) {
-		// 5. Iterate through each spark
-
+		// 5. Iterate through each spark //
+		for (int p = 0; p < sparks.length; p++) {
+			sparks[p]= new Spark(100,100);
+		//sparks[p]= g.setColor();
+		//sparks[p]= g.getColor();
+		g.setColor(Color.BLACK);
+		}
 		// 6. Set the color to the spark's color with g.setColor()
-
+		
 		// 7. Fill in an oval using the spark's x, y, and size with g.fillOval()
+		g.fillOval(50,50,100,100);
 	}
 }
--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -5,22 +5,50 @@ import java.util.Random;
 public class MoreArrayFun {
 	//1. Create a main method to test the other methods you write.
 	
+public static void main(String[]args) {
+	MoreArrayFun yes= new MoreArrayFun();
+	String[] ues= new String[56];
+	//The purpose of making an instance is to call nonstatic methods. 
+	yes.setNames(ues);
+	yes.seteveryother(ues);
+	yes.reverseprint(ues);
 	
-	
-	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
-	
-	
-	
-	//3. Write a method that takes an array of Strings and prints all the Strings in the array
-	//   in reverse order.
-	
-	
-	
-	//4. Write a method that takes an array of Strings and prints every other String in the array.
-	
-	
-	//5. Write a method that takes an array of Strings and prints all the Strings in the array
+}
+//2. Write a method that takes an array of Strings and prints all the Strings in the array.
+void setNames(String[] names2) {
+	//String names[]= new String[6];
+	for (int i = 0; i < names2.length; i++) {
+	System.out.println(names2[i]);
+	//The parameters in the methods are there so I can put in a general value for any of them. 
+	}
+}
+//3. Write a method that takes an array of Strings and prints all the Strings in the array in reverse order.
+void reverseprint(String[] names2) {
+	for (int n = 0; n < names2.length; n++) {
+	names2[n]= "Jar";
+	System.out.println(names2[n]);	
+	}
+}
+//4. Write a method that takes an array of Strings and prints every other String in the array.
+void seteveryother(String[]names2) {
+	for (int j = 0; j < names2.length; j+=2) {
+	names2[j]= "I will";	
+	System.out.println(names2[j]);
+	}
+}
+
+//5. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in a completely random order. Almost every run of the program should result in a different order.
 	
 	
-}
+	
+	
+	
+	}
+	
+	
+
+	
+	
+	
+
